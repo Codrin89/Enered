8dca34536e852a055abdd68239668ba1fe686196
Supernatural100 <vanomancer10@gmail.com>
2017-04-19 21:34:43 +0300

    Curs 11

---
+++ Curs11/index.js
@@ -0,0 +1,48 @@
+(function() {
+
+var z = {
+	date: "12.07.2017",
+	sum: "190 RON",
+	points: "44",
+	movie:"Piratii din Caraibe",
+	description: "Fain filmu"
+}
+
+for(var i = 0 ; i < 20 ; i++) {
+	createRow(z);
+}
+
+function createRow(z) {
+var row = document.createElement('tr');
+
+var date = document.createElement('td');
+var dateText = document.createTextNode(z.date);
+date.append(dateText);
+
+var sum = document.createElement('td');
+var sumText = document.createTextNode(z.sum);
+sum.append(sumText);
+
+var points = document.createElement('td');
+var pointsText = document.createTextNode(z.points);
+points.append(pointsText);
+
+var movie = document.createElement('td');
+var movieText = document.createTextNode(z.movie);
+movie.append(movieText);
+
+var description = document.createElement('td');
+var descriptionText = document.createTextNode(z.description);
+description.append(descriptionText);
+
+row.append(date);
+row.append(sum);
+row.append(points);
+row.append(movie);
+row.append(description);
+
+document.getElementsByTagName('tbody')[0].append(row);
+}
+
+
+})();
