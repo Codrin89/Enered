d54981e36b02579f4b8f64a2c87e7ccec60f0bc6
Web <aliviuandrei@gmail.com>
2017-04-26 21:24:14 +0300

    Curs 26.04.2017

---
+++ Curs 26.04.2017/index.js
@@ -0,0 +1,27 @@
+(function() {
+
+
+
+	console.log($('#clickBtn'));
+	$('#clickBtn').click(function() {
+		// alert("Butonul a fost apasat!!!")
+		$('.animate') .css('display', 'block');
+		$('.animate') .addClass('animate-position');
+
+
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
