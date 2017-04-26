49b2ef917d76fa2b2dcb8d2a51e7ae476af7648c
Diana-Web <dianamaria.web@gmail.com>
2017-04-26 21:23:13 +0300

    curs 26.04.2017

---
+++ curs 26.04.2017/index.js
@@ -0,0 +1,31 @@
+(function() {
+
+
+console.log($('#clickBtn'));
+$('#clickBtn').click(function(){
+	// alert('butonul a fost apasat');
+	$('.animate').css('display', 'block');
+	$('.animate').addClass('animate-position');
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
