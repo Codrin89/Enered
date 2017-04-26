f82a9df23674e7154369120d8bdd54956cd2793a
Supernatural100 <vanomancer10@gmail.com>
2017-04-26 21:40:22 +0300

    Curs13

---
+++ Curs13/index.js
@@ -0,0 +1,21 @@
+(function() {
+
+
+	console.log($('#clickBtn'));
+	$('#clickBtn').click(function() {
+		alert('Butonul a fost apasat!!!');
+			$('.animate').css('display', 'block');
+			$('.animate').addClass('animate-color');
+			$('.animate').addClass('animate-position');
+	});
+
+	$ (document).click(function(event) {
+		$('.circle').css('top', event.pageY);
+		$('.circle').css('left', event.pageX);
+	});
+
+
+
+
+
+})();
