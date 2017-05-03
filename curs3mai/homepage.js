2daff5859182307b4ff250c6eab99cf9e110f9b6
tanasa-amalia <amalia.tanasa1@gmail.com>
2017-05-03 21:35:21 +0300

    curs3mai

---
+++ curs3mai/homepage.js
@@ -0,0 +1,14 @@
+(function() {
+		var time = setTimeout(function() {
+ 						window.location.href = "Index.html";
+ 					},1000);
+
+        
+	})();
+
+
+
+
+
+//addEventLIstener//click..dbl_click..keydown..keypress..mouseleave..focusout..mouseenter..focusin..change
+//style.property//
---
+++ curs3mai/index.js
@@ -0,0 +1,80 @@
+(function() {
+
+	document.getElementById('openInfo').addEventListener('click' , function() {
+		  var info =  document.getElementsByClassName('info')[0];
+	        //document.getElementsByClassName('info')[0].style.display = "block";
+	          // info.style.display = "block";
+	           	  //console.log(info.style.display);
+	           	     if (info.style.display == "block") {
+	           	     	info.style.display = "none";
+	           	     }else{
+	           	     	info.style.display = "block";
+	           	     }
+
+
+	});
+
+
+				var d = new Date();
+				var hour = d.getHours();
+ 				
+ 				if(hour > 11 && hour < 18) {
+ 					document.getElementById('timeofday').innerHTML = "Afternoon";
+ 				}
+ 				if((hour > 17 && hour < 24) || (hour => 0 && hour <4)) {
+ 					document.getElementById('timeofday').innerHTML = "Evening";
+ 				}
+ 				if(hour > 5 && hour < 10) {
+ 					document.getElementById('timeofday').innerHTML = "Morning";
+ 				}
+
+ 				function checkBrowser() {
+ 					// Opera 8.0+
+var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
+// Firefox 1.0+
+var isFirefox = typeof InstallTrigger !== 'undefined';
+
+// Safari 3.0+ "[object HTMLElementConstructor]" 
+var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
+
+// Internet Explorer 6-11
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+// Edge 20+
+var isEdge = !isIE && !!window.StyleMedia;
+
+// Chrome 1+
+var isChrome = !!window.chrome && !!window.chrome.webstore;
+console.log(isChrome);
+
+
+// Blink engine detection
+var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+ 				}
+ 				checkBrowser();
+ 				document.getElementById('next-page').addEventListener('click' , function() {
+ 					var time = setTimeout(function(){
+ 						window.location.href = "Index.html"
+ 					},1000);
+ 					var set = setInterval(function() {
+ 						window.location.href = "Index.html";
+ 					},1000);
+
+    })
+ 				document.getElementById('timeofday').style.opacity = 1;
+ 				var set = setInterval(function() {
+
+ 					
+ 						
+ 						var timer = document.getElementById('timeofday');
+ 						timer.style.opacity = timer .style.opacity - 0.2;
+ 						if(timer.style.opacity == 0) {
+ 						clearInterval(set)
+
+ 					   };
+ 					},1000);
+
+
+})(); 
