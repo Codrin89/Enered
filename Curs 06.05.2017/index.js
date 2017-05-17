ed41d7794ab2a006ffcf5497ef726c3d9b85bbcf
Web <aliviuandrei@gmail.com>
2017-05-17 22:15:46 +0300

    curs 17.05.2017

---
+++ Curs 06.05.2017/index.js
@@ -0,0 +1,25 @@
+(function(){
+
+
+	var img = document.getElementsByClassName("img-responsive");
+	
+
+	for(var i=0; i<img.length; i++){
+		img[i].addEventListener('click', function(){
+			var img = document.getElementsByClassName("img-responsive");
+			var index;
+	for(var i=0; i<img.length; i++){
+		if(img[i]==this){
+			index=i;
+		}
+
+	}
+	if(this.src==)		
+			this.src='Lighthouse.jpg';
+		});
+	};
+
+
+
+
+})();
