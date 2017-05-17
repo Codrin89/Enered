8cd3bd507a418d236030657448aa6691ba79447b
Supernatural100 <vanomancer10@gmail.com>
2017-05-17 21:46:47 +0300

    17Mai

---
+++ 17Mai/index.js
@@ -0,0 +1,21 @@
+(function () {
+	document.getElementsByClassName('account')[0].getElementsByClassName('btn-gol')[0].addEventListener('click', function() {
+
+		var dropDown = this.parentElement.getElementsByClassName('dropdown-content')[0];
+
+		if(dropDown.style.display == 'block') {
+			dropDown.style.display = 'none';
+		} else {
+			dropDown.style.display = 'block';
+		}
+		// this.parentElement.getElementsByClassName('dropdown-content')[0].style.display = 'block';
+	});
+
+	var tabs = document.getElementsByClassName('tabs-step');
+	for(var i = 0 ; i < tabs.length ; i++) {
+		tabs[i].addEventListener('click' , function() {
+			document.getElementsByClassName('active')[0].classList.remove('active');
+			this.classList.add('active');
+		});
+	}
+})();
