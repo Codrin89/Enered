d658dc4ffea484c26fceaf4a9bb002f75d7b9e7e
tanasa-amalia <amalia.tanasa1@gmail.com>
2017-05-24 21:59:18 +0300

    curs24mai

---
+++ curs24mai/index.js
@@ -0,0 +1,25 @@
+(function(){
+
+	var right = document.getElementsByClassName('glyphicon-play');
+	for(var i = 0; i< right.length; i++){
+		right[i].addEventListener('click', function() {
+			if(this.getAttribute('data-click') == "true"){
+				console.log('true');
+				var info = this.parentElement.parentElement.getElementsByClassName('info-text');
+				for(var i = 0; i < info.length; i++){
+					info[i].style.display = 'none';
+				}
+				this.setAttribute('data-click', 'false');
+			}else{
+				var info = this.parentElement.parentElement.getElementsByClassName('info-text');
+				for(var i = 0; i < info.length; i++){
+				info[i].style.display = 'block';
+			}
+			this.setAttribute('data-click', 'true');
+		  }
+
+		});
+
+	 }
+
+})();
