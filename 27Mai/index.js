6883e95e7cd529921e188ac2a73954ff83756a53
Supernatural100 <vanomancer10@gmail.com>
2017-05-27 14:27:45 +0300

    27Mai

---
+++ 27Mai/index.js
@@ -0,0 +1,57 @@
+(function () {
+	// Intrebarea 1 si 2
+	// var person = prompt("Please enter your name"); 		
+		// if (person != null) {
+    		// document.getElementById("hello").innerHTML ="What's is your input string?" + person;
+    		// document.getElementById('p').innerHTML = person + ' has ' + person.length + ' characters ';
+// };
+
+	// Problema 3
+
+	// var quote = prompt('What is the quote');
+		// document.getElementById('1').innerHTML = "What is the quote?" + quote;
+	// var who = prompt('Who said it?');
+		// document.getElementById('answer').innerHTML = 'Who said it?' + who;
+		// document.getElementById('done').innerHTML = who + ' says, ' + quote;
+
+		// Problema 4
+
+		// var noun = prompt('Enter a noun:');
+			// document.getElementById('a').innerHTML = 'Enter a noun:' + noun;
+		// var verb = prompt ('Enter a verb : ');
+			// document.getElementById('b').innerHTML = 'Enter a verb :' + verb;
+		// var adjective = prompt ('Enter an adjective:');
+			// document.getElementById('c').innerHTML = 'Enter an adjective:' + adjective;
+		// var	adverb = prompt ('Enter an adverb:');
+			// document.getElementById('d').innerHTML = 'Enter an adverb:' + adverb;
+			// document.getElementById('done').innerHTML = ' Do you ' + verb + ' your ' + adjective  + ' '+ noun + ' '+ adverb + ' ? ' + ' That is hilarious! ';	
+
+			// Problema 5
+
+			// var a = prompt("What is the first number?");
+			// var b = prompt("What is the second number?");
+			// document.getElementById('a').innerHTML = 'What is the first number?' + a;
+			// document.getElementById('b').innerHTML = 'What is the second number?' + b;
+			// a = parseInt(a);
+			// b = parseInt(b);
+			// document.getElementById('result').innerHTML = a + ' + '  + b +' = ' +  (a + b) ;
+			// document.getElementById('result1').innerHTML = a + ' - ' + b + ' = ' + (a - b) ;
+			// document.getElementById('result2').innerHTML = a + ' * ' + b + ' = ' +  (a * b) ;
+			// document.getElementById('result3').innerHTML = a + ' / ' + b + ' = ' +  (a / b) ;
+
+			// Problema 6
+			
+			// var Age = prompt("What is your current age?");
+			// var Yearsretire = prompt("At what age would you like to retire?");
+			// document.getElementById('a').innerHTML = 'What is your current age?' + Age;
+			// document.getElementById('b').innerHTML = 'At what age would you like to retire?' + Yearsretire;
+			// document.getElementById('result').innerHTML = ' You have ' + (Yearsretire - Age)  +  'left until you can retire.';
+			// document.getElementById('result1').innerHTML = 'Its 2017 , so you can retire in' + (2017 + (Yearsretire - Age)) + ' . ';
+
+			//  Problema 7
+
+			
+
+
+
+})();
