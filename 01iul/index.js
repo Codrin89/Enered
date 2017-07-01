c82ede685d7fe2bfda09b7483045cd9bc18b2629
iuliann84 <iuliann84@gmail.com>
2017-07-01 13:42:35 +0300

    1iul

---
+++ 01iul/index.js
@@ -0,0 +1,95 @@
+(function(){
+	var data = {
+	"items": [
+       {
+       		"id": 4132,
+       		"Email": "johnishappy@johntheguy.com",
+       		"FirstName": "John",
+       	    "LastName": "Doe",
+       	    "PhotoUrl": "assets/pic1.jpg",
+       	    "CreationDate": "2h"
+       },
+       {
+       		"id": 5132,
+       		"Email": "criscatinca@catinca.com",
+       		"FirstName": "Cristina",
+       	    "LastName": "Catinca",
+       	    "PhotoUrl": "assets/pic2.jpg",
+       	    "CreationDate": "2h"
+       },
+       {
+       		"id": 4132,
+       		"Email": "johnishappy@johntheguy.com",
+       		"FirstName": "John",
+       	    "LastName": "Doe",
+       	    "PhotoUrl": "assets/pic1.jpg",
+       	    "CreationDate": "2h"
+       },
+       {
+       		"id": 5132,
+       		"Email": "criscatinca@catinca.com",
+       		"FirstName": "Cristina",
+       	    "LastName": "Catinca",
+       	    "PhotoUrl": "assets/pic2.jpg",
+       	    "CreationDate": "2h"
+       },
+       {
+       		"id": 4132,
+       		"Email": "johnishappy@johntheguy.com",
+       		"FirstName": "John",
+       	    "LastName": "Doe",
+       	    "PhotoUrl": "assets/pic1.jpg",
+       	    "CreationDate": "2h"
+       },
+       {
+       		"id": 5132,
+       		"Email": "criscatinca@catinca.com",
+       		"FirstName": "Cristina",
+       	    "LastName": "Catinca",
+       	    "PhotoUrl": "assets/pic2.jpg",
+       	    "CreationDate": "2h"
+       },
+       {
+       		"id": 4132,
+       		"Email": "johnishappy@johntheguy.com",
+       		"FirstName": "John",
+       	    "LastName": "Doe",
+       	    "PhotoUrl": "assets/pic1.jpg",
+       	    "CreationDate": "2h"
+       },
+       {
+       		"id": 5132,
+       		"Email": "criscatinca@catinca.com",
+       		"FirstName": "Cristina",
+       	    "LastName": "Catinca",
+       	    "PhotoUrl": "assets/pic2.jpg",
+       	    "CreationDate": "2h"
+       },
+       {
+       		"id": 4132,
+       		"Email": "johnishappy@johntheguy.com",
+       		"FirstName": "John",
+       	    "LastName": "Doe",
+       	    "PhotoUrl": "assets/pic1.jpg",
+       	    "CreationDate": "2h"
+       },
+       {
+       		"id": 5132,
+       		"Email": "criscatinca@catinca.com",
+       		"FirstName": "Cristina",
+       	    "LastName": "Catinca",
+       	    "PhotoUrl": "assets/pic2.jpg",
+       	    "CreationDate": "2h"
+       },
+       {
+       		"id": 4132,
+       		"Email": "johnishappy@johntheguy.com",
+       		"FirstName": "John",
+       	    "LastName": "Doe",
+       	    "PhotoUrl": "assets/pic1.jpg",
+       	    "CreationDate": "2h"
+       }
+	]
+}
+})();
+ 
